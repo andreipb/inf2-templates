328bd24d5e04ac64a0d5df54322ba8554a64c807
Andrei Popescu-Belis <andreipb@free.fr>
2019-03-06 22:04:05 +0100

    Add maximum.h

---
+++ maximum.h
@@ -0,0 +1,16 @@
+/* 
+ * File:   maximum.h
+ * Author: Andrei
+ *
+ * Created on 6. mars 2019, 08:59
+ */
+
+#ifndef MAXIMUM_H
+#define MAXIMUM_H
+
+template <typename T> T maximum(T x, T y) {
+    return (x>y)?x:y;
+}
+
+#endif /* MAXIMUM_H */
+

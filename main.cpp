0fe5568e6643cc8e1c5766a164e7d977a2cd46e8
Andrei Popescu-Belis <andreipb@users.noreply.github.com>
2019-03-06 20:17:59 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrei
+ * But : illustrer la compilation séparée et l'instanciation des templates.
+ * Created on 6. mars 2019, 08:34
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include <typeinfo>
+#include "minimum.h"
+#include "maximum.h"
+
+using namespace std;
+
+template int maximum<int>(int, int); 
+// on peut combiner une initialisation
+// explicite (ici à 'int') et implicite (plus bas à 'float')
+
+// template float minimum<float>(float, float); // impossible
+
+int main() {
+    {
+        int a = 3; // 'int' est le seul type accepté ici
+        int b = 2; // 'minimum<float>' n'aide pas, car la déf. est dans .cpp
+        cout << "Minimum de a et b : " << minimum(a, b)
+                << " de type : " << typeid (minimum(a, b)).name() << endl;
+    }
+    
+    {
+        float a = 3.f;
+        float b = 2.f;
+        cout << "Maximum de a et b : " << maximum(a, b)
+                << " de type : " << typeid (maximum(a, b)).name() << endl;
+    }
+
+    {
+        char a = 'H';
+        int b = 'G';  // warning à la compilation mais possible grâce à 
+                      // l'initialisation explicite du template avec un type
+        cout << "Maximum de a et b : " << maximum<char>(a, b)
+                << " de type : " << typeid (maximum<char>(a, b)).name() << endl;
+    }
+
+    return EXIT_SUCCESS;
+}

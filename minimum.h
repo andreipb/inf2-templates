04af0be3e853ec3a92f96738b207615db217a03a
Andrei Popescu-Belis <andreipb@users.noreply.github.com>
2019-03-06 20:31:06 +0100

    Create minimum.h

---
+++ minimum.h
@@ -0,0 +1,13 @@
+/* 
+ * File:   minimum.h
+ * Author: Andrei
+ *
+ * Created on 6. mars 2019, 08:35
+ */
+
+#ifndef MINIMUM_H
+#define MINIMUM_H
+
+template <typename T> T minimum(T, T);
+
+#endif /* MINIMUM_H */
